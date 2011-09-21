f490f8a6b0cdf4eaa7ffe97a19969794a436aac6
Hervé <treizetech@gmail.com>
2011-09-21 21:39:10 +0200

    up

--- download_year.sh
+++ download_year.sh
@@ -2,4 +2,4 @@
 # Ce script récupère les photos d'une année depuis s3 vers $PHOTOS
 # Attention, le répertoire de l'année doit être présent
 
-../s3sync/s3sync.rb $2 -rsv --delete --exclude="Picasa|Thumbs" boudala:photos/$1/ $PHOTOS/$1
+../s3sync/s3sync.rb $2 -rsv --delete --exclude="Picasa|Thumbs" $S3_ACCOUNT:photos/$1/ $PHOTOS/$1
